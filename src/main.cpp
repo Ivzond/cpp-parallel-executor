ce414d27875855703f0bf10b4871d47a85650c4e
Ivzond <G9207807332@yandex.ru>
2023-12-07 20:14:31 +0300

    Added command line input

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,14 @@
 
 int main(int argc, char * argv[])
 {
-    int loop_A = 0, loop_B = 0, crush_A = -1, crush_B = -1;
-    std::cout << "Input iterations for device A, device B: ";
-    std::cin >> loop_A >> loop_B;
-    std::cout << "Input crush indices for device A, device B (-1 by default): ";
-    std::cin >> crush_A >> crush_B;
+    int loop_A = std::stoi(argv[1]);
+    int loop_B = std::stoi(argv[2]);
+    int crush_A = std::stoi(argv[3]);
+    int crush_B = std::stoi(argv[4]);
 
-    std::shared_ptr<EventQueue> queue = std::make_shared<EventQueue>();
-    std::shared_ptr<Device> A = std::make_shared<DeviceA>();
-    std::shared_ptr<Device> B = std::make_shared<DeviceB>();
+    auto queue = std::make_shared<EventQueue>();
+    auto A = std::make_shared<DeviceA>();
+    auto B = std::make_shared<DeviceB>();
 
     Parser parser(queue, A, B);
 

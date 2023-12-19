578a21603570ebd2f997b98efd0e11715131994e
Ivzond <141146338+Ivzond@users.noreply.github.com>
2023-12-19 11:16:58 +0300

    Update DataEvent.cpp

--- src/DataEvent.cpp
+++ src/DataEvent.cpp
@@ -2,9 +2,6 @@
 #include <iostream>
 #include <utility>
 
-DataEvent::DataEvent(std::shared_ptr<Device> device)
-: DeviceEvent(std::move(device)) {}
-
 std::string DataEvent::toString() const {
     return device->getDataAsString() + " from " + device->getName();
 }

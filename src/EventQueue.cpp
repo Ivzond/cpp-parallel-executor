d9419088f4dcab17c0f4270f42aa53ca1b9a8143
Ivzond <141146338+Ivzond@users.noreply.github.com>
2023-12-19 12:45:32 +0300

    Update EventQueue.cpp
    Bug fix

--- src/EventQueue.cpp
+++ src/EventQueue.cpp
@@ -9,13 +9,12 @@ void EventQueue::push(const std::shared_ptr<const Event>& event) {
 std::shared_ptr<const Event> EventQueue::pop(const std::chrono::seconds& duration) {
     std::unique_lock<std::mutex> lock(mutex);
     while (queue.empty()) {
-        condition_variable.wait_for(lock, duration);
+        if (condition_variable.wait_for(lock, duration) == std::cv_status::timeout) {
+            // Return nullptr if the queue is empty after waiting for the specified duration
+            return nullptr;
+        }
     }
-    if (!queue.empty()) {
-        auto event = queue.front();
-        queue.pop();
-        return event;
-    }
-// Return nullptr if the queue is empty after waiting for the specified duration
-    return nullptr;
+    auto event = queue.front();
+    queue.pop();
+    return event;
 }
